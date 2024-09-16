8c423347de96ec38ca54dfe0322d93c4caf79531
Victor Coriolano <94132818+victorcoriolano@users.noreply.github.com>
2024-09-16 12:33:24 -0300

    ajustando logicas de snackbar e posicionando os métodos, validando as imagens

--- lib/ui/pages/imc_calc.dart
+++ lib/ui/pages/imc_calc.dart
@@ -57,79 +57,96 @@ class _IMCCalculatorState extends State<IMCCalculator> {
   Future<void> _calcularIMC() async {
     late double? peso = double.tryParse(_pesoController.text);
     late double? altura = double.tryParse(_alturaController.text);
-
+    late double imc;
+    String estadoSaude;
+    //Valida o formulário
     Map<String?, double?> imcValues = FormValidator.validateForm(
         peso, altura, _alturaController, _pesoController, context);
-
+    //Verifica valores nulos devolvidos pelo validator
     if (imcValues['peso'] == null || imcValues['altura'] == null) {
       setState(() {
         _resultadoIMC = 'Insira valores para conferir seu IMC!';
       });
       return;
     }
+    //Acata valores devolvidos pelo validator
     if (imcValues['peso'] != null && imcValues['altura'] != null) {
-      if (imcValues['peso']! < 2 ||
-          imcValues['altura']! < 40 ||
-          imcValues['peso']! > 600 ||
-          imcValues['altura']! > 300 ||
-          imcValues['altura']! == 0) {
-        SnackBar snackBar = const SnackBar(
-          content: Text(
-            'Os valores parecem inválidos. Tente colocar seu peso em quilos e sua altura em centímetros (Ex: 70, 180).',
-          ),
-          duration: Duration(seconds: 7),
-          elevation: 1,
-          padding: EdgeInsets.all(10),
-          showCloseIcon: false,
-          behavior: SnackBarBehavior.floating,
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        return;
-      }
       peso = imcValues['peso'];
       altura = imcValues['altura'];
     }
-
-    final double imc = peso! / ((altura! / 100) * (altura / 100));
-
-    String estadoSaude;
-
-    if (imc < 16) {
-      estadoSaude = 'Magreza Grau II';
-      _fotoCachorro =
-          'https://cdn.leroymerlin.com.br/products/escultura_madeira_cachorro_baleia_em_pe_olhando_baixo_mar_1571790414_dd86_600x600.png';
-    } else if (imc < 17) {
-      estadoSaude = 'Magreza Grau II';
-      _fotoCachorro =
-          'https://i1.sndcdn.com/artworks-000249285635-edgnte-t500x500.jpg';
-    } else if (imc < 18.5) {
-      estadoSaude = 'Magreza Grau I';
-      _fotoCachorro =
-          'https://blog-static.petlove.com.br/wp-content/uploads/2020/06/galgo-petlove.jpg';
-    } else if (imc < 25) {
-      estadoSaude = 'Saudável';
-      _fotoCachorro =
-          'https://t1.uc.ltmcdn.com/pt/posts/7/0/1/por_que_os_cachorros_gostam_de_carinho_na_barriga_25107_600.jpg';
-    } else if (imc < 30) {
-      estadoSaude = 'Sobrepeso';
-      _fotoCachorro =
-          'https://media.istockphoto.com/id/803485968/pt/foto/fat-beagle.jpg?s=612x612&w=0&k=20&c=FIC4cUwk_wjVoozRdWAOswhJqixIWoGNxvICAtJQqbk=';
-    } else if (imc < 35) {
-      estadoSaude = 'Obesidade Moderada (grau I)';
-      _fotoCachorro =
-          'https://blog-static.petlove.com.br/wp-content/uploads/2024/05/10144002/cachorro-gordo-Petlove.jpg';
-    } else if (imc < 40) {
-      estadoSaude = 'Obesidade Severa (grau II)';
-      _fotoCachorro =
-          'https://ogimg.infoglobo.com.br/in/13168881-e59-b8a/FT1086A/obie.jpg';
+    //Verifica valores exorbitantes
+    if (imcValues['peso']! < 2 ||
+        imcValues['altura']! < 40 ||
+        imcValues['peso']! > 600 ||
+        imcValues['altura']! > 300 ||
+        imcValues['altura']! == 0) {
+      SnackBar snackBar = const SnackBar(
+        content: Text(
+          'Os valores parecem inválidos. Tente colocar seu peso em quilos e sua altura em centímetros (Ex: 70, 180).',
+        ),
+        duration: Duration(seconds: 4),
+        elevation: 1,
+        padding: EdgeInsets.all(10),
+        showCloseIcon: false,
+        behavior: SnackBarBehavior.floating,
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      return;
+      // Valores inválidos acima
+      //Cálculo e estado de saúde/foto abaixo
     } else {
-      estadoSaude = 'Obesidade Muito Severa (grau III)';
-      _fotoCachorro =
-          'https://s2.glbimg.com/wFqgUL8IGb4N_U2470KOgVaeXms=/620x465/top/s.glbimg.com/jo/g1/f/original/2016/12/16/bolinha_1700_agora.jpg';
+      imc = peso! / ((altura! / 100) * (altura / 100));
+      if (imc < 16) {
+        estadoSaude = 'Magreza Grau III';
+        _fotoCachorro =
+            'https://dcdn.mitiendanube.com/stores/002/234/113/products/img_65051-74bb310bbddda32c9116764061136419-240-0.jpg';
+      } else if (imc < 17) {
+        estadoSaude = 'Magreza Grau II';
+        _fotoCachorro =
+            'https://i1.sndcdn.com/artworks-000249285635-edgnte-t500x500.jpg';
+      } else if (imc < 18.5) {
+        estadoSaude = 'Magreza Grau I';
+        _fotoCachorro =
+            'https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/09/05/1922043128-greyhound.jpg';
+      } else if (imc < 25) {
+        estadoSaude = 'Saudável';
+        _fotoCachorro =
+            'https://t1.uc.ltmcdn.com/pt/posts/7/0/1/por_que_os_cachorros_gostam_de_carinho_na_barriga_25107_600.jpg';
+      } else if (imc < 30) {
+        estadoSaude = 'Sobrepeso';
+        _fotoCachorro =
+            'https://media.istockphoto.com/id/803485968/pt/foto/fat-beagle.jpg?s=612x612&w=0&k=20&c=FIC4cUwk_wjVoozRdWAOswhJqixIWoGNxvICAtJQqbk=';
+      } else if (imc < 35) {
+        estadoSaude = 'Obesidade Moderada (grau I)';
+        _fotoCachorro =
+            'https://static.wixstatic.com/media/69f465_7c003c94721e4008a84805cb55271247~mv2.jpg/v1/fill/w_600,h_398,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/69f465_7c003c94721e4008a84805cb55271247~mv2.jpg';
+      } else if (imc < 40) {
+        estadoSaude = 'Obesidade Severa (grau II)';
+        _fotoCachorro =
+            'https://ogimg.infoglobo.com.br/in/13168881-e59-b8a/FT1086A/obie.jpg';
+      } else {
+        estadoSaude = 'Obesidade Muito Severa (grau III)';
+        _fotoCachorro =
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQekQ4Dy1bjCvs5lLsiUg-ORFViMexSAXBWGCxfcSujoPSeyGNnLqhmCTFhyLOcQkMvqx8&usqp=CAU';
+      }
+      setState(() {
+        //Caso o usuário escreva "1.70", ele vai ser considerado como 170
+        if (_alturaController.text.substring(1, 2) == '.') {
+          SnackBar snackBar = SnackBar(
+            content: Text(
+              'A altura foi considerada como $altura por conter ponto flutuante após uma casa (Ex: 1.7). Insira valores extensos para uma avaliação precisa (Ex: 174 ou 174.5)!',
+            ),
+            duration: const Duration(seconds: 2),
+            elevation: 1,
+            padding: const EdgeInsets.all(10),
+            showCloseIcon: false,
+            behavior: SnackBarBehavior.floating,
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        }
+        _resultadoIMC = 'Seu IMC é: ${imc.toStringAsFixed(1)}\n$estadoSaude';
+      });
     }
-    setState(() {
-      _resultadoIMC = 'Seu IMC é: ${imc.toStringAsFixed(1)}\n$estadoSaude';
-    });
   }
 
   void _insertText(String text) {
@@ -140,6 +157,7 @@ class _IMCCalculatorState extends State<IMCCalculator> {
         _alturaController.text += text;
       }
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
     _calcularIMC();
   }
 
--- lib/ui/widgets/form_validator.dart
+++ lib/ui/widgets/form_validator.dart
@@ -10,34 +10,8 @@ class FormValidator {
     if (peso != null && altura != null && altura > 0 && peso > 0) {
       if (alturaController.text.substring(1, 2) == '.') {
         altura *= 100;
-        SnackBar snackBar = SnackBar(
-          content: Text(
-            'A altura foi considerada como $altura por conter ponto flutuante após uma casa (Ex: 1.7). Insira valores extensos para uma avaliação precisa (Ex: 174 ou 174.5)!',
-          ),
-          duration: const Duration(seconds: 7),
-          elevation: 1,
-          padding: const EdgeInsets.all(10),
-          showCloseIcon: false,
-          behavior: SnackBarBehavior.floating,
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
       }
-      if (peso < 2.5 && altura < 40) {
-        SnackBar snackBar = const SnackBar(
-          content: Text(
-            'O IMC foi calculado, mas os valores parecem inválidos. Tente colocar seu peso em quilos e sua altura em centímetros (Ex: 70, 180).',
-          ),
-          duration: Duration(seconds: 7),
-          elevation: 1,
-          padding: EdgeInsets.all(10),
-          showCloseIcon: false,
-          behavior: SnackBarBehavior.floating,
-        );
-        ScaffoldMessenger.of(context).showSnackBar(snackBar);
-        return {'peso': null, 'altura': null};
-      } else {
-        return {'peso': peso, 'altura': altura};
-      }
+      return {'peso': peso, 'altura': altura};
     } else {
       return {'peso': null, 'altura': null};
     }
